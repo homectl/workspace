f043fdccc35e9839ace3a4ffd2df0c6121916e33
Pippijn van Steenhoven <pip88nl@gmail.com>
2021-04-18 01:58:48 +0100

    Add `matrix*vec` test.

--- GPipe-Core/test/Graphics/GPipe/Debugger/EvalSpec.hs
+++ GPipe-Core/test/Graphics/GPipe/Debugger/EvalSpec.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Graphics.GPipe.Debugger.EvalSpec where
 
@@ -6,19 +6,24 @@ import           Test.Hspec                      (Spec, describe, it, shouldBe)
 import           Test.Hspec.QuickCheck           (prop)
 
 import qualified Data.Text.Lazy                  as LT
-import           Graphics.GPipe
 import qualified Graphics.GPipe.Debugger.Compile as Compile
 import qualified Graphics.GPipe.Debugger.Eval    as Eval
 import           Graphics.GPipe.Debugger.Value   (Value (..))
+import           Graphics.GPipe.Expr             (FFloat)
+import           Graphics.GPipe.Linear           (V3 (..), V4 (..), identity,
+                                                  norm, signorm, (!*))
 
 compile :: FFloat -> IO LT.Text
 compile = Compile.compile
 
+compileV4 :: V4 FFloat -> IO LT.Text
+compileV4 = Compile.compile
+
 eval :: LT.Text -> Either String Value
 eval = Eval.eval
 
-ffloat :: Int -> FFloat
-ffloat = fromIntegral
+ffloat :: Float -> FFloat
+ffloat = fromRational . toRational
 
 
 spec :: Spec
@@ -44,9 +49,18 @@ spec = do
         prop "should return a vector of length 1" $ \case
             (0, 0, 0) -> return ()
             (x, y, z) -> do
-                let r = norm
-                        . signorm
-                        $ V3 (ffloat x) (ffloat y) (ffloat z)
+                let r = norm . signorm $ V3 (ffloat x) (ffloat y) (ffloat z)
                 code <- compile r
                 let res = eval code
                 res `shouldBe` Right (FloatValue 1)
+
+    describe "!*" $ do
+        prop "should work on the identity matrix" $ \case
+            (0, 0, 0) -> return ()
+            (x, y, z) -> do
+                let m = identity
+                    v = V4 (ffloat x) (ffloat y) (ffloat z) 1
+                    r = m !* v
+                code <- compileV4 r
+                let res = eval code
+                res `shouldBe` Right (Vec4Value (V4 x y z 1))
